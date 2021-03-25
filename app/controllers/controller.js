b585081623559ed3ba472ca2fc7db6f64a455cb2
Luis Angel Peña <lapena@mavi.mx>
2021-03-25 13:37:29 -0600

    feat:: Preload iyected contacts data

--- app/controllers/controller.js
+++ app/controllers/controller.js
@@ -1,7 +1,9 @@
 angular.module('plunkerApp')
 .controller('ContactsCtrl',
-    function($scope, ContactsService) {
-        $scope.listContacts = [
-            {_id: 1, name: 'Allan Benjamin', password:'lol1', phone: '557188339933', address: 'St. Claire Avenue, Nº 101'},
-            {_id: 2, name: 'Georgia Smith', password:'lol2', phone: '557188339933', address: 'St. Claire Avenue, Nº 102'}];
+    function($scope, ContactsService, listContacts) {
+        $scope.filteredData = [];
+        $scope.init = function() {
+            $scope.filteredData = listContacts;
+        }
+        $scope.init();
     });
