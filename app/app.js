e029953f092893278399c39d800d681e159000de
Luis Angel Peña <lapena@mavi.mx>
2021-03-25 12:53:22 -0600

    feat:: Add the routing between the app and views

--- app/app.js
+++ app/app.js
@@ -1,1 +1,23 @@
-angular.module('plunkerApp',[]);
+angular.module('plunkerApp',[
+    'ngRoute', // Adding `angular-route.js`
+])
+.config(function ( $locationProvider) {
+    $locationProvider.html5Mode(true);
+})
+.config(function ($routeProvider) {
+    $routeProvider
+        // Default route
+        .when('/', {
+            templateUrl: 'views/list/list.html',
+            controller: 'ContactsCtrl'
+        })
+        // add a new contact
+        .when('/contacts/new', {
+            templateUrl: 'views/new.html',
+            controller: 'ContactsCtrl'
+        })
+        // Redirect to main route
+        .otherwise({
+          redirectTo: '/'
+        });
+});
