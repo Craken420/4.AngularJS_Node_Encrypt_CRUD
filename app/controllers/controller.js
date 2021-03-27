504007af6f04a12882753074921aa4b2d6aacbff
Luis Angel Peña <lapena@mavi.mx>
2021-03-26 19:35:19 -0600

    feat:: Add th delete contact function

--- app/controllers/controller.js
+++ app/controllers/controller.js
@@ -5,27 +5,25 @@ angular.module('plunkerApp')
         * initial value of creation/alteration contact form
         * @type {Array}
         */
-         $scope.contact = [],
-       
-         /**
-         * Reinitialize form values
-         */
-         $scope.reset = function() {
-             $scope.contact = [
-                 {
-                     name: '',
-                     address: '',
-                     phone: ''
-                 }
-             ];
+        $scope.contact = [],
+        /**
+        * Reinitialize form values
+        */
+        $scope.reset = function() {
+            $scope.contact = [
+                {
+                    name: '',
+                    address: '',
+                    phone: ''
+                }
+            ];
          };
          /**
-         * Add um contato
+         * Add contact
          */
          $scope.create = function(contact) {
-             listContacts = ContactsService.create(contact);
-         };
-
+            listContacts = ContactsService.create(contact);
+         }
          $scope.save = function(item) {
             if(typeof item._id !== 'undefined')
                 $scope.update(item);
@@ -50,30 +48,44 @@ angular.module('plunkerApp')
             listContacts = ContactsService.update(item);
         };
         /**
-        * Add pagination to contacts list
+        * Remove a contact of contact list
+        * @param  {Integer} index        `_id` value's contact
+        * @return {Boolean}
         */
-        $scope.currentPage = 1,
-        $scope.numPerPage = 3,
-        $scope.maxSize = 5;
-
-        $scope.$watch('currentPage + numPerPage + search', function() {
-            var begin = ( ($scope.currentPage - 1) * $scope.numPerPage),
-                end = begin + $scope.numPerPage;
-
-            if ($scope.search)
-                $scope.filteredData = $filter('filter')(listContacts,
-                    {name: $scope.search}).slice(begin, end);
-            else
-                $scope.filteredData = listContacts.slice(begin, end);
-        });
+         $scope.delete = function(index) {
+            item = ContactsService.delete(index);
+            if (!!item) {
+                $scope.init();
+                return true;
+            }
+            return false;
+        };
 
         $scope.init = function() {
-            listContacts.concat($scope.filteredDataPages);
+            listContacts = ContactsService.getListItems();
+            $scope.filteredData = listContacts;
+            /**
+            * Add pagination to contacts list
+            */
+            $scope.currentPage = 1,
+            $scope.numPerPage = 3,
+            $scope.maxSize = 5;
+
+            $scope.$watch('currentPage + numPerPage + search', function() {
+                var begin = ( ($scope.currentPage - 1) * $scope.numPerPage),
+                    end = begin + $scope.numPerPage;
+
+                if ($scope.search)
+                    $scope.filteredData = $filter('filter')(listContacts,
+                        {name: $scope.search}).slice(begin, end);
+                else
+                    $scope.filteredData = listContacts.slice(begin, end);
+            });
             $scope.reset();
             if ($route.current.method !== undefined) {
                 $scope[$route.current.method]();
             }
-            $scope.totalItems = (listContacts.length * 10)/$scope.numPerPage;
+            $scope.totalItems = (listContacts.length * 10) / $scope.numPerPage;
         }
         $scope.init();
     });
--- app/services/service.js
+++ app/services/service.js
@@ -3,6 +3,13 @@ angular.module('plunkerApp')
     var _items = listContacts;
 
     return {
+        setListItems: function(items) {
+          _items = items;
+          return this;
+        },
+        getListItems: function() {
+            return _items;
+        },
         create: function (item) {
           item._id = _items.length;
           _items.push(item);
@@ -17,6 +24,23 @@ angular.module('plunkerApp')
               return element;
             });
             return _items;
-        }
+        },
+        delete: function(index) {
+          var db = this.getListItems();
+          var _id = db.filter( function (element, pos) {
+            if ( element._id === index) {
+              element.pos = pos;
+              return element;
+            }
+          });
+          if (_id.length > 0) {
+            var item = db.splice(_id[0].pos, 1);
+            if (typeof item[0] === 'object') {
+              this.setListItems(db);
+              return item[0];
+            }
+          }
+          return false;
+      }
     }
 })
